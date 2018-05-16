345c4f146315b8bd8b02c655eb91560e471da211
jhassler1031 <jhassler1031@gmail.com>
2018-05-16 13:26:30 -0400

    second iter of cleanup, start of dictionary creation

--- word-freq.py
+++ word-freq.py
@@ -3,7 +3,20 @@ with open("sample2.txt") as infile:
     my_file = infile.read()
 
 #Need to normalize all words in the file (remove numbers and punctuation)
+
 my_file = my_file.lower()
+
+x = 0
+
+while x < len(my_file):
+    if my_file[x].isalpha() or my_file[x] == " ":
+        x += 1
+    else:
+        y = my_file[x]
+        my_file = my_file.replace(y , "")
+
+#below is what was originally used for cleanup
+"""
 
 my_file_list = my_file.split()
 
@@ -20,8 +33,19 @@ while index_counter < len(my_file_list):
             word = word.replace(y , "")
     my_file_list[index_counter] = word
     index_counter += 1
+"""
 
+#var my_file should be clean at this point
 
+#turn str my_file into iterable list, then add list items as keys and counter occurances
 
+my_file_list = my_file.split()
 
-print(my_file_list)
+word_count = {}
+
+for word in my_file_list:
+    if word not in word_count.keys():
+        word_count[word] = 0
+    word_count[word] += 1
+
+print(word_count)
