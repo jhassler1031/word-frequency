cbef9b6499086771eb84a7d5138ceeeac27b3092
jhassler1031 <jhassler1031@gmail.com>
2018-05-16 14:14:47 -0400

    sorted the list and printed

--- word-freq.py
+++ word-freq.py
@@ -6,6 +6,12 @@ with open("sample2.txt") as infile:
 
 my_file = my_file.lower()
 
+#below is to change periods to spaces so to separate words after next phase of cleanup
+
+my_file = my_file.replace( "." , " ")
+
+#remove remaining punctuation and numbers
+
 x = 0
 
 while x < len(my_file):
@@ -15,27 +21,6 @@ while x < len(my_file):
         y = my_file[x]
         my_file = my_file.replace(y , "")
 
-#below is what was originally used for cleanup
-"""
-
-my_file_list = my_file.split()
-
-index_counter = 0
-
-while index_counter < len(my_file_list):
-    x = 0
-    word = my_file_list[index_counter]
-    while x < len(word):
-        if word[x].isalpha():
-            x += 1
-        else:
-            y = word[x]
-            word = word.replace(y , "")
-    my_file_list[index_counter] = word
-    index_counter += 1
-"""
-
-#var my_file should be clean at this point
 
 #turn str my_file into iterable list, then add list items as keys and counter occurances
 
@@ -48,4 +33,15 @@ for word in my_file_list:
         word_count[word] = 0
     word_count[word] += 1
 
-print(word_count)
+#now have a dictionary of words with the count of how many times they are used
+
+#now need to sort by occurance and print the top 20 words
+#source for operator: https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+import operator
+
+sorted_list = sorted(word_count.items(), key=operator.itemgetter(1), reverse = True)
+
+for item in sorted_list:
+    print(item)
+    
